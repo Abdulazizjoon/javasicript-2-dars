355f3afa96f745c512a57231e31125f02e505e01
Пользователь <email manzilingiz>
2024-03-28 19:37:51 +0500

    first commit

---
+++ 1/my.js
@@ -0,0 +1,7 @@
+let a = prompt('bor pulingizni somasi kiriting')
+if (a>=1200) {
+    console.log('oq yol, Alisher');
+}
+else{
+    console.log('Alisher ozgina sabr qilish kerak bolar ekan');
+}
---
+++ 2/index.js
@@ -0,0 +1,22 @@
+let a = prompt('1dan 7gacha son kiritin')
+if (a=1) {
+    console.log('dushanba')
+}
+else if(a=2){
+    console.log('seshanba')
+}
+else if(a=3){
+    console.log('chorshanba')
+}
+else if(a=4){
+    console.log('payshanba')
+}
+else if(a=5){
+    console.log('juma')
+}
+else if(a=6){
+    console.log('shanba')
+}
+else if(a=7){
+    console.log('yakshanba')
+}
